9292b238c9b1181bc4268c7f19b6807f72a7a9bd
asadullah48 <asadullahshafique@hotmail.com>
2024-05-25 15:51:54 +0500

    Initial commit

---
+++ ans84.js
@@ -0,0 +1,10 @@
+// Q 84
+// This function swaps "JavaScript" with "TypeScript" in a sentence
+function replaceJavaScriptWithTypeScript(sentence) {
+    return sentence.replace(/JavaScript/g, "TypeScript");
+    // Uses a regular expression with the 'g' flag for a global replacement
+}
+console.log(replaceJavaScriptWithTypeScript("I am learning JavaScript and JavaScript is awesome!"));
+// Example: Changing a programming language name in a sentence
+// Outputs: "I love TypeScript and TypeScript is awesome!"
+// Every "JavaScript" in the sentence is changed to "TypeScript".
